ba949bafae16c9e95138d826375d1d25d5f7a691
Teera W <arm.wijitsirisopon@education.nsw.gov.au>
2026-03-24 19:40:04 +1100

    update

---
+++ TEGetGo.github.io-main/README.md
@@ -0,0 +1,2 @@
+# TEGetGo.github.io
+This is a web site that meant to be for start up affiliate market
---
+++ TEGetGo.github.io-main/js/main.js
@@ -0,0 +1,15 @@
+const contactEmail = document.querySelectorAll(".contact-email");
+
+function copyEmail() {
+  navigator.clipboard.writeText('tegetgoofficial@gmail.com');
+  document.getElementById('copy-msg').style.display = 'block';
+  setTimeout(() => {
+    document.getElementById('copy-msg').style.display = 'none';
+  }, 2000);
+}
+
+contactEmail.forEach(email => {
+  email.addEventListener('click', () => {
+    copyEmail();
+  });
+});
